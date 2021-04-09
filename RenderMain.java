8b45c6eb72c49af55906605b565a8fd2d4138aa2
ICantRanch <81833066+ICantRanch@users.noreply.github.com>
2021-04-09 17:04:47 -0400

    Add files via upload

--- RenderMain.java
+++ RenderMain.java
@@ -1,142 +1,184 @@
 package JavaRenderer;
 
-import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
-
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.geometry.Orientation;
+import javafx.geometry.Point3D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.LineTo;
-import javafx.scene.shape.MoveTo;
-import javafx.scene.shape.Path;
+import javafx.scene.shape.*;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Transform;
 import javafx.stage.Stage;
+
+import java.util.*;
 
 public class RenderMain extends Application {
 
-	int WIN_WIDTH = 400;
-	int WIN_HEIGHT = 400;
-	Canvas c;
-	GraphicsContext gc;
-	Timer t;
-	TimerTask tt;
-	Slider lR;
-	Group center;
-	Path[] pArr;
-	
-	ArrayList<Triangle> tris;
-	
-	public void start(Stage stage) {
-		
-		BorderPane root = new BorderPane();
-		Scene scene = new Scene(root);
-		c = new Canvas(WIN_WIDTH,WIN_HEIGHT);
-		gc = c.getGraphicsContext2D();
-		center = new Group();
-		root.setCenter(center);
-		
-		lR = new Slider(0,360,180);
-		root.setBottom(lR);
-		lR.valueProperty().addListener(new ChangeListener<Number>() {
+    int WIN_WIDTH = 500;
+    int WIN_HEIGHT = 500;
+    Canvas c;
+    GraphicsContext gc;
+    Timer t;
+    TimerTask tt;
+    Slider lR;
+    Slider uD;
+    Group center;
+    Circle ci;
+    Path[] pArr;
+    double fakeHeading = 0;
+    double fakePitch = 0;
+    boolean isTimerRun =false;
+    Rotate hRotate = new Rotate(0, new javafx.geometry.Point3D(0,1,0));
+    Rotate pRotate = new Rotate(0,new javafx.geometry.Point3D(1,0,0));
 
-			public void changed(
-					ObservableValue<? extends Number> observableValue,
-					Number oldNum,
-					Number newNum) {
-				paint();
-			}
-		});
-		
-		gc.translate(WIN_WIDTH/2, WIN_HEIGHT/2);
-		
-		tris = new ArrayList<Triangle>();
-		tris.add(new Triangle(new Vertex(100, 100, 100),
-		                      new Vertex(-100, -100, 100),
-		                      new Vertex(-100, 100, -100),
-		                      Color.WHITE));
-		tris.add(new Triangle(new Vertex(100, 100, 100),
-		                      new Vertex(-100, -100, 100),
-		                      new Vertex(100, -100, -100),
-		                      Color.RED));
-		tris.add(new Triangle(new Vertex(-100, 100, -100),
-		                      new Vertex(100, -100, -100),
-		                      new Vertex(100, 100, 100),
-		                      Color.GREEN));
-		tris.add(new Triangle(new Vertex(-100, 100, -100),
-		                      new Vertex(100, -100, -100),
-		                      new Vertex(-100, -100, 100),
-		                      Color.BLUE));
-		
-		pArr = new Path[tris.size()];
-		for (int i = 0; i < pArr.length; i++) {
-			pArr[i] = new Path();
-		}
-		
-		
-		paint();
-		
-		
-		
-		stage.setHeight(WIN_HEIGHT);
-		stage.setWidth(WIN_WIDTH);
-		stage.setTitle("Renderer");
-		stage.setScene(scene);
-		stage.show();
-		
-		
-	}
-	
-	public void paint() {
-		
-		gc.setFill(Color.WHITE);
-		
-		
-		double heading = Math.toRadians(lR.getValue());
-		Matrix3 transMat = new Matrix3(new double[][] {
-			{Math.cos(heading),0,-Math.sin(heading)},
-			{0,1,0},
-			{Math.sin(heading),0,Math.cos(heading)}
-			});
-		
-		
-		for (int i = 0; i < tris.size(); i++) {
-			
-			Vertex v1 = transMat.transform(tris.get(i).getV1());
-			Vertex v2 = transMat.transform(tris.get(i).getV2());
-			Vertex v3 = transMat.transform(tris.get(i).getV3());
-			
-			Path p = pArr[i];
-			p.getElements().clear();
-			p.setFill(tris.get(i).getColor());
-			p.getElements().add(new MoveTo(v1.getX(),v1.getY()));
-			p.getElements().add(new LineTo(v2.getX(),v2.getY()));
-			p.getElements().add(new LineTo(v3.getX(),v3.getY()));
-			center.getChildren().add(p);
-			
-			
-			
-		}
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		launch();
+    ArrayList<Triangle> tris;
+    Image img;
 
-	}
+    public void start(Stage stage) {
+
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root);
+        center = new Group();
+        root.setCenter(center);
+        c = new Canvas(WIN_WIDTH, WIN_HEIGHT);
+
+        lR = new Slider(-180,180,0);
+        uD = new Slider(-90,90,0);
+        uD.setOrientation(Orientation.VERTICAL);
+        root.setBottom(lR);
+        root.setRight(uD);
+
+        lR.valueProperty().addListener((observableValue, oldNum, newNum) -> paint());
+        uD.valueProperty().addListener((observableValue, number, t1) -> paint());
+
+
+
+        tris = new ArrayList<Triangle>();
+        tris.add(new Triangle(new Point3D(100, 100, 100),
+                new Point3D(-100, -100, 100),
+                new Point3D(-100, 100, -100),
+                Color.WHITE));
+        tris.add(new Triangle(new Point3D(100, 100, 100),
+                new Point3D(-100, -100, 100),
+                new Point3D(100, -100, -100),
+                Color.RED));
+        tris.add(new Triangle(new Point3D(-100, 100, -100),
+                new Point3D(100, -100, -100),
+                new Point3D(100, 100, 100),
+                Color.LIMEGREEN));
+        tris.add(new Triangle(new Point3D(-100, 100, -100),
+                new Point3D(100, -100, -100),
+                new Point3D(-100, -100, 100),
+                Color.MEDIUMBLUE));
+
+        pArr = new Path[tris.size()];
+        for (int i = 0; i < pArr.length; i++) {
+            pArr[i] = new Path();
+
+            pArr[i].getElements().add(new MoveTo(tris.get(i).getV1().getX(),tris.get(i).getV1().getY()));
+            pArr[i].getElements().add(new LineTo(tris.get(i).getV2().getX(),tris.get(i).getV2().getY()));
+            pArr[i].getElements().add(new LineTo(tris.get(i).getV3().getX(),tris.get(i).getV3().getY()));
+            pArr[i].getElements().add(new ClosePath());
+            pArr[i].setFill(tris.get(i).getColor());
+
+            center.getChildren().add(pArr[i]);
+        }
+        ci = new Circle(0,0,2);
+        center.getChildren().add(ci);
+
+        System.out.println(hRotate.getAxis());
+
+        //rotate();
+        paint();
+
+        stage.setHeight(WIN_HEIGHT);
+        stage.setWidth(WIN_WIDTH);
+        stage.setTitle("Renderer");
+        stage.setScene(scene);
+        stage.show();
+
+        stage.setOnCloseRequest(event ->{
+            if(isTimerRun){
+                t.cancel();
+            }
+        });
+    }
+
+    public void rotate() {
+
+        tt = new TimerTask() {
+
+            @Override
+            public void run() {
+
+                isTimerRun = true;
+                paint(fakeHeading,fakePitch);
+                fakeHeading += 1.5;
+                fakePitch += 1.5;
+
+            }
+        };
+        t = new Timer();
+        t.schedule(tt, 0, 34);
+    }
+
+    public void paint() {
+
+        double heading = lR.getValue();
+        double pitch = uD.getValue();
+        paint(heading,pitch);
+    }
+
+    public void paint(double hDegrees, double pDegrees) {
+
+        hRotate.setAngle(-hDegrees);
+        pRotate.setAngle(-pDegrees);
+        Transform trans = hRotate.createConcatenation(pRotate);
+        double[][] newOrder = new double[tris.size()][2];
+        double average;
+        double sum;
+
+        for (int i = 0; i < tris.size(); i++) {
+
+
+            Point3D v1 = trans.transform(tris.get(i).getV1());
+            Point3D v2 = trans.transform(tris.get(i).getV2());
+            Point3D v3 = trans.transform(tris.get(i).getV3());
+
+            sum = v1.getZ() + v2.getZ() + v3.getZ();
+            average = sum/3;
+            newOrder[i] = new double[]{average, i};
+
+            Path p = pArr[i];
+            p.getElements().clear();
+
+            p.getElements().add(new MoveTo(v1.getX(),v1.getY()));
+            p.getElements().add(new LineTo(v2.getX(),v2.getY()));
+            p.getElements().add(new LineTo(v3.getX(),v3.getY()));
+            p.getElements().add(new ClosePath());
+            //p.getTransforms().addAll(hRotate,pRotate);
+
+            System.out.println(v1);
+        }
+        System.out.println(Arrays.deepToString(newOrder));
+        Arrays.sort(newOrder, Comparator.comparingDouble(o -> o[0]));
+        System.out.println(Arrays.deepToString(newOrder));
+        for (int i = 0; i < newOrder.length; i++) {
+            pArr[(int)newOrder[i][1]].toFront();
+        }
+        ci.toFront();
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        launch();
+
+    }
 
 }
--- Triangle.java
+++ Triangle.java
@@ -1,53 +1,62 @@
 package JavaRenderer;
 
+import javafx.geometry.Point3D;
 import javafx.scene.paint.Color;
 
 public class Triangle {
 
-	Vertex v1;
-	Vertex v2;
-	Vertex v3;
-	Color color;
-	
-	
+    Point3D v1;
+    Point3D v2;
+    Point3D v3;
+    Color color;
+    int numVertex;
 
-	public Triangle(Vertex v1, Vertex v2, Vertex v3, Color color) {
-		this.v1 = v1;
-		this.v2 = v2;
-		this.v3 = v3;
-		this.color = color;
-	}
-	
-	public Vertex getV1() {
-		return v1;
-	}
 
-	public void setV1(Vertex v1) {
-		this.v1 = v1;
-	}
 
-	public Vertex getV2() {
-		return v2;
-	}
+    public Triangle(Point3D v1, Point3D v2, Point3D v3, Color color) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+        this.color = color;
+        int numVertex = 3;
+    }
 
-	public void setV2(Vertex v2) {
-		this.v2 = v2;
-	}
+    public int getNumVertex() { return numVertex; }
 
-	public Vertex getV3() {
-		return v3;
-	}
+    public Point3D[] getVertices(){
+        return new Point3D[]{v1,v2,v3};
+    }
 
-	public void setV3(Vertex v3) {
-		this.v3 = v3;
-	}
+    public Point3D getV1() {
+        return v1;
+    }
 
-	public Color getColor() {
-		return color;
-	}
+    public void setV1(Point3D v1) {
+        this.v1 = v1;
+    }
 
-	public void setColor(Color color) {
-		this.color = color;
-	}
-	
+    public Point3D getV2() {
+        return v2;
+    }
+
+    public void setV2(Point3D v2) {
+        this.v2 = v2;
+    }
+
+    public Point3D getV3() {
+        return v3;
+    }
+
+    public void setV3(Point3D v3) {
+        this.v3 = v3;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
 }
